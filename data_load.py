0a79f0091b962544e519da903d5e2734db61118e
DataMathematician <datamathematician@gmail.com>
2021-02-10 16:55:09 +0300

    0.0.2

--- data_load.py
+++ data_load.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from perceptron import Perceprton
+from decision_regions import plot_decision_regions
 
 data = pd.read_csv('iris.csv')
 data = data[data['Species'].isin(['Iris-setosa','Iris-versicolor'])]
@@ -18,7 +19,9 @@ plt.scatter(X[:50, 0],X[:50, 1], color='red', marker = 'o', label='setosa')
 plt.scatter(X[50:, 0],X[50:, 1], color='blue', marker = 'o', label='versicolor')
 plt.legend()
 
-ppn = Perceprton(eta = 0.1,n_iter=10)
+ppn = Perceprton(eta = 0.1,n_iter=5)
 ppn.fit(X,y)
 
 plt.plot(range(1,len(ppn.errors_) + 1), ppn.errors_,marker='o')
+
+plot_decision_regions(X,y,classifier=ppn)
