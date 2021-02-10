0ce7bda93a7c47cf98dd0935464b740cd365205b
DataMathematician <datamathematician@gmail.com>
2021-02-10 15:58:50 +0300

    0.0.1
    Perceptron

---
+++ data_load.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from perceptron import Perceprton
+
+data = pd.read_csv('iris.csv')
+data = data[data['Species'].isin(['Iris-setosa','Iris-versicolor'])]
+data = data.drop('Id',axis=1)
+data
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = data.loc[:,'Species'].values # .values -> array instead of pd.Series()
+y = np.where(y == 'Iris-setosa',-1,1) # if y == 'Iris-setosa' y=-1 else y=1
+
+X = data.iloc[:,[0,2]].values
+
+plt.scatter(X[:50, 0],X[:50, 1], color='red', marker = 'o', label='setosa')
+plt.scatter(X[50:, 0],X[50:, 1], color='blue', marker = 'o', label='versicolor')
+plt.legend()
+
+ppn = Perceprton(eta = 0.1,n_iter=10)
+ppn.fit(X,y)
+
+plt.plot(range(1,len(ppn.errors_) + 1), ppn.errors_,marker='o')
